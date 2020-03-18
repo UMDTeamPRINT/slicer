368627256e1f580904a85173194450d8e52602ca
Boo1098 <github@s-d.space>
2020-03-18 16:27:16 -0400

    added requirements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # slicer
+requirements:
+[Fast Stl Import Function](https://www.mathworks.com/matlabcentral/fileexchange/30923-fast-stl-import-function)
--- test.m
+++ test.m
@@ -1,4 +1,4 @@
-[P,T,N]=stlread("structure.stl",1);
+[P,T,N]=import_stl_fast("structure.stl",1);
 
 patch('Faces', T, 'Vertices', P, 'FaceVertexCData', (1:length(T(:,1)))', 'FaceColor', 'flat');
 view(3)
