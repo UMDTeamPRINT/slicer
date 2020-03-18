9a91aa6daf1b7c195d124569e524158178f8203e
Boo1098 <github@s-d.space>
2020-03-18 16:46:08 -0400

    implemented simple mesh and rotate

--- test.m
+++ test.m
@@ -1,5 +1,9 @@
 [P,T,N]=import_stl_fast("structure.stl",1);
 
+[P, N] = rotate_mesh(P,N,[-90 0 0],1);
+P = translate_mesh(P,[0 0 0]);
+
 patch('Faces', T, 'Vertices', P, 'FaceVertexCData', (1:length(T(:,1)))', 'FaceColor', 'flat');
 view(3)
 axis equal
+zlabel('Z');xlabel('X');ylabel('Y');
