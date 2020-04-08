bb8a5155af703c88767c6fe50c8e8ec4c9b286d6
Boo1098 <nathansd1098@gmail.com>
2020-04-08 00:51:28 -0400

    added documentation and explicit form

--- raise_layer.m
+++ raise_layer.m
@@ -1,13 +1,18 @@
 function [To,Po] = raise_layer(t,T,P)
+
 Po=[];
-syms x y z;
+
 for i=1:size(T,1)
     tic
     adj=[];
     tt = T(i,:);
+    
+    % Find adjacent triangles
     for j=1:size(T,1)
         if j~=i
             % ismember is slow when dealing with small arrays.
+            % instead check directly if triangle is adjacent through brute
+            % force means.
             sum = 0;
             for k=1:3
                 for u=1:3
@@ -22,35 +27,45 @@ for i=1:size(T,1)
         end
     end
     
-    % Save 4 triangles' points
+    % Save triangles raw points values
+    % Don't try to understand it, it's stupid
     ts(:,:,1) = P(sub2ind(size(P),[tt(1,:);tt(1,:);tt(1,:)]',[1:3;1:3;1:3]));
     ts(:,:,2) = P(sub2ind(size(P),[adj(1,:);adj(1,:);adj(1,:)]',[1:3;1:3;1:3]));
     ts(:,:,3) = P(sub2ind(size(P),[adj(2,:);adj(2,:);adj(2,:)]',[1:3;1:3;1:3]));
     ts(:,:,4) = P(sub2ind(size(P),[adj(3,:);adj(3,:);adj(3,:)]',[1:3;1:3;1:3]));
     
     
+    % Project  points
     ns = zeros(4,3);
     rts = zeros(3,3,4);
     for j=1:4
+        % Find normal of triangle and scale to thickness
         tt = ts(:,:,j);
         n = cross((tt(2,:)-tt(1,:)),(tt(3,:)-tt(1,:)));
         n = n/norm(n);
         ns(j,:) = t*n;
-
+        
+        % Save raised triangles
         rts(:,:,j) = ts(:,:,j)+[ns(j,:);ns(j,:);ns(j,:)];
-
-        %ps(:,j) = 0==dot(ns(j,:),[x y z]-rts(1,:,j));
     end
     
+    % Find line of each plane intersect with original triangle
     point = zeros(4,3);
     dir = zeros(4,3);
     for j=2:4
         [point(j,:), dir(j,:)] = plane_intersect(ns(1,:),rts(1,:,j),ns(j,:),rts(1,:,j));
     end
     
+    % Uses x^2 to get intersection. Need to find explicit form.
     np(1,:) = lineXline([point(2,:);point(3,:)],[point(2,:)+dir(2,:);point(3,:)+dir(3,:)]);
     np(2,:) = lineXline([point(3,:);point(4,:)],[point(3,:)+dir(3,:);point(4,:)+dir(4,:)]);
     np(3,:) = lineXline([point(4,:);point(2,:)],[point(4,:)+dir(4,:);point(2,:)+dir(2,:)]);
+    
+    % Uses exact form. fails for any singularity
+    % TODO figure out how to fix for singularities
+    np(1,:) = point(2,:)+dir(3,:)*((dir(3,2)*point(2,3) - dir(3,3)*point(2,2) - dir(3,2)*point(3,3) + dir(3,3)*point(3,2))/(dir(2,2)*dir(3,3) - dir(3,2)*dir(2,3)));
+    np(2,:) = point(3,:)+dir(4,:)*((dir(4,2)*point(3,3) - dir(4,3)*point(3,2) - dir(4,2)*point(4,3) + dir(4,3)*point(4,2))/(dir(3,2)*dir(4,3) - dir(4,2)*dir(3,3)));
+    np(3,:) = point(4,:)+dir(2,:)*((dir(2,2)*point(4,3) - dir(2,3)*point(4,2) - dir(2,2)*point(2,3) + dir(2,3)*point(2,2))/(dir(4,2)*dir(2,3) - dir(2,2)*dir(4,3)));
     
     Po = [Po;np];
     %toc
