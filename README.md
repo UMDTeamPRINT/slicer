f7e8a8c41b5c80547444d059beccd2283fe9f022
Boo1098 <nathansd1098@gmail.com>
2020-04-08 00:57:38 -0400

    updated dependencies

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Requirements:
 - [gridtrimesh](https://www.mathworks.com/matlabcentral/fileexchange/14646-gridtrimesh)
 - [inpolyhedron](https://www.mathworks.com/matlabcentral/fileexchange/37856-inpolyhedron-are-points-inside-a-triangulated-volume)
 - [plane_intersect](https://www.mathworks.com/matlabcentral/fileexchange/17618-plane-intersection)
+- [lineXline](https://www.mathworks.com/matlabcentral/fileexchange/59805-line-line-intersection-n-lines-d-space)
--- raise_layer.m
+++ raise_layer.m
@@ -63,9 +63,9 @@ for i=1:size(T,1)
     
     % Uses exact form. fails for any singularity
     % TODO figure out how to fix for singularities
-    np(1,:) = point(2,:)+dir(3,:)*((dir(3,2)*point(2,3) - dir(3,3)*point(2,2) - dir(3,2)*point(3,3) + dir(3,3)*point(3,2))/(dir(2,2)*dir(3,3) - dir(3,2)*dir(2,3)));
-    np(2,:) = point(3,:)+dir(4,:)*((dir(4,2)*point(3,3) - dir(4,3)*point(3,2) - dir(4,2)*point(4,3) + dir(4,3)*point(4,2))/(dir(3,2)*dir(4,3) - dir(4,2)*dir(3,3)));
-    np(3,:) = point(4,:)+dir(2,:)*((dir(2,2)*point(4,3) - dir(2,3)*point(4,2) - dir(2,2)*point(2,3) + dir(2,3)*point(2,2))/(dir(4,2)*dir(2,3) - dir(2,2)*dir(4,3)));
+    %np(1,:) = point(2,:)+dir(3,:)*((dir(3,2)*point(2,3) - dir(3,3)*point(2,2) - dir(3,2)*point(3,3) + dir(3,3)*point(3,2))/(dir(2,2)*dir(3,3) - dir(3,2)*dir(2,3)));
+    %np(2,:) = point(3,:)+dir(4,:)*((dir(4,2)*point(3,3) - dir(4,3)*point(3,2) - dir(4,2)*point(4,3) + dir(4,3)*point(4,2))/(dir(3,2)*dir(4,3) - dir(4,2)*dir(3,3)));
+    %np(3,:) = point(4,:)+dir(2,:)*((dir(2,2)*point(4,3) - dir(2,3)*point(4,2) - dir(2,2)*point(2,3) + dir(2,3)*point(2,2))/(dir(4,2)*dir(2,3) - dir(2,2)*dir(4,3)));
     
     Po = [Po;np];
     %toc
