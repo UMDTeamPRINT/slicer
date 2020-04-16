a456ce49ed16629627ddaa1f850bdc9df15994e6
Boo1098 <nathansd1098@gmail.com>
2020-04-16 18:33:13 -0400

    created raise method that averages normals

---
+++ mean_raise.m
@@ -0,0 +1,32 @@
+function Po = mean_layer(t,T,P,N)
+Po=zeros(size(P));
+for i=1:size(P,1)
+    
+    p=P(i,:);
+    
+    ns = zeros(6,3);
+    
+    % Find attached triangles
+    cn=1;
+    for j=1:size(T,1)
+        % ismember is slow when dealing with small arrays.
+        % instead check directly if triangle is adjacent through brute
+        % force means.
+        for k=1:3
+            if T(j,k)==i
+                ns(cn,:)=N(j,:)/norm(N(j,:));
+                cn=cn+1;
+            end
+        end
+    end
+    
+    avgn = zeros(1,3);
+    for j=1:6
+        avgn=avgn+ns(i,:);
+    end
+    
+    avgn = t*avgn/norm(avgn);
+    
+    Po(i,:) = p+avgn;
+end
+end
