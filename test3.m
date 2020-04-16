0f7d7d7aebb55210176a92ab01332be9d9011bee
Boo1098 <nathansd1098@gmail.com>
2020-04-16 18:38:00 -0400

    created tester file for mean_raise

---
+++ test3.m
@@ -0,0 +1,52 @@
+%% Input values
+structure_stl = "structure.stl";
+print_stl = "print.stl";
+
+% How much to translate the structure
+ts = [0 0 0];
+% How much to rotate the structure
+rs = [-90 0 0];
+
+% How much to translate the print
+tp = [0 0 50.8-22];
+% How much to rotate the print
+rp = [-90 0 0];
+
+% 1 means all inputs are in degrees
+deg = 1;
+
+% resolution in X,Y
+r = 0.2;
+
+% layer height
+lh = 0.2;
+
+%% Import STLs
+% Structure
+[Ps,Ts,Ns]=import_stl_fast(structure_stl,1);
+
+[Ps, Ns] = rotate_mesh(Ps,Ns,rs,deg);
+Ps = translate_mesh(Ps,ts);
+
+% Print
+[Pp,Tp,Np]=import_stl_fast(print_stl,1);
+
+[Pp,Np] = rotate_mesh(Pp,Np,rp,deg);
+Pp = translate_mesh(Pp,tp);
+
+%% Display models
+% figure
+% hold on
+% patch('Faces', Ts, 'Vertices', Ps, 'FaceVertexCData', (1:length(Ts(:,1)))', 'FaceColor', 'flat');
+% view(3)
+% axis equal
+% zlabel('Z');xlabel('X');ylabel('Y');
+% patch('Faces', Tp, 'Vertices', Pp, 'FaceVertexCData', (1:length(Tp(:,1)))', 'FaceColor', 'flat');
+% hold off
+
+%%
+Ppl = mean_raise(5,Ts,Ps,Ns);
+plot3(Ppl(:,1),Ppl(:,2),Ppl(:,3),'o');
+xlim([-100 100]);ylim([-100 100]);zlim([-100 100]);
+hold on
+patch('Faces', Ts, 'Vertices', Ps, 'FaceVertexCData', (1:length(Ts(:,1)))', 'FaceColor', 'flat');
